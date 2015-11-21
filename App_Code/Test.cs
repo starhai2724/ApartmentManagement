b920e43ee73852282df2c0d1bd9a65b2fcc66024
starhai2724 <hai2724@gmail.com>
2015-11-21 16:50:20 +0700

    hai

---
+++ App_Code/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Test
+/// </summary>
+public class Test
+{
+    public Test()
+    {
+    }
+
+    static void Main(string[] args)
+    {
+        /* my first program in C# */
+        Console.WriteLine("Hello World");
+        Console.ReadKey();
+    }
+
+}
