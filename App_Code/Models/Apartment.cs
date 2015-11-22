8537ce10988fc6582c5dd671522b879d5d52f7e8
starhai2724 <hai2724@gmail.com>
2015-11-22 22:36:11 +0700

    create model

---
+++ App_Code/Models/Apartment.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Apartment
+/// </summary>
+namespace models
+{
+
+    public class Apartment
+    {
+        int idApartment { get; set; }
+        string nameApartment { get; set; }
+        string typeApartment { get; set; }
+        string size { get; set; }
+        double priceSale { get; set; }
+        double priceRent { get; set; }
+        int statusApartment { get; set; }
+        string dateCreate { get; set; }
+        string userCreate { get; set; }
+        string dateUpdate { get; set; }
+        string userUpdate { get; set; }
+
+
+
+        public Apartment()
+        {
+
+        }
+    }
+}
---
+++ App_Code/Models/GenericDataAccess.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Apartment.Models
+{
+    public class GenericDataAccess
+    {
+        public static SqlCommand CreateCommand()
+        {
+            string connectString =
+                WebConfigurationManager.ConnectionStrings["qlch01"].ConnectionString;
+            SqlConnection connection = new SqlConnection(connectString);
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+            return command;
+        }
+
+        public static DataTable ExecuteSelectCommand(SqlCommand command)
+        {
+            DataTable table;
+            try
+            {
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                table = new DataTable();
+                table.Load(reader);
+                reader.Close();
+            }
+            finally { command.Connection.Close(); }
+            return table;
+        }
+
+        public static int ExecuteNoneQuery(SqlCommand command)
+        {
+            int affectedRows = -1;
+            try
+            {
+                command.Connection.Open();
+                affectedRows = command.ExecuteNonQuery();
+            }
+            finally { command.Connection.Close(); }
+            return affectedRows;
+        }
+
+        public static string ExecuteScalar(SqlCommand command)
+        {
+            string value = "";
+            try
+            {
+                command.Connection.Open();
+                value = command.ExecuteScalar().ToString();
+            }
+            finally { command.Connection.Close(); }
+            return value;
+        }
+    }
+}
